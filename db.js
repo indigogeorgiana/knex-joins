18cb218d57963d4ebd7f4eb2a2a073a80a840ffd
Rachael Peri <rachael.peri@gmail.com>
2018-06-12 11:27:00 +1200

    womble characteristics

--- db.js
+++ db.js
@@ -3,10 +3,18 @@ const config = require('./knexfile').development
 const db = knex(config)
 
 module.exports = {
-  getWombles
+  getWombles,
+  getWombleInfo
 }
 
 function getWombles () {
   return db('wombles')
     .select('wombles.name')
+}
+
+function getWombleInfo (id) {
+  return db('wombles')
+    .join('characteristics', 'characteristics.id', 'wombles.characteristic_id')
+    .select('wombles.name', 'characteristics.description')
+    .where('wombles.id', id)
 }
--- routes.js
+++ routes.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/view/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getWombleInfo(id)
+    .then(womble => {
+      const wombleData = {
+        name: womble[0].name,
+        womble: womble
+      }
+      console.log(wombleData)
+      res.render('view', wombleData)
+    })
+    .catch(err => {
+      res.send(err.message).status(500)
+    })
+})
+
 module.exports = router
